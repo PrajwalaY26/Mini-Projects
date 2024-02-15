3e2e6788e1553bde8ab8920d1ffce3d2b696f943
PrajwalaYadlapalli <117836562+PrajwalaYadlapalli@users.noreply.github.com>
2024-02-15 19:53:19 +0530

    Removed Telegram_Bot directory

--- Telegram_Bot/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
